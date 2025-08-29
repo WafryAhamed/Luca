7f4c13e9485f9b62fb18f6ab6eb96e623d02e0e6
Wafry Ahamed <wafry07@gmail.com>
2025-08-30 01:56:59 +0530

    interface -  import styles, add CSS classes and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
-function App() {
-  
+import styles from './App.module.css';
 
+function App() {
   return (
-     <div>
-      luca-chatbot
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/robot-Luca.svg" />
+        <h2 className={styles.Title}>AI Chatbot</h2>
+      </header>
+      <div className={styles.ChatContainer} />
     </div>
-    
   );
 }
 
-export default App
+export default App;

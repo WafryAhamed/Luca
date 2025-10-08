fd044116342f1625bc03c3deeac3a327f279786d
Wafry Ahamed <wafry07@gmail.com>
2025-10-08 16:08:32 +0530

    chat update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 
+import { useState } from 'react';
+import Chat from './components/chat/chat';
 import styles from './App.module.css';
 
+
 function App() {
+  const[messages, setMessages] = useState([]);
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
         <img className={styles.Logo} src="/robot-Luca.svg" />
         <h2 className={styles.Title}>AI Chatbot</h2>
       </header>
-      <div className={styles.ChatContainer} />
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
+      </div>
     </div>
   );
 }
